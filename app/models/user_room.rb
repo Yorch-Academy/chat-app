5b341f920eded37916537b76adc94931987e9f9c
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-02-26 18:03:13 -0600

    Displaying rooms and render controller.

--- app/models/user_room.rb
+++ app/models/user_room.rb
@@ -1,12 +1,4 @@
 class UserRoom < ApplicationRecord
   belongs_to :user
   belongs_to :room
-
-  # after_create_commit :broadcast_rooms_create
-
-  private
-
-  # def broadcast_rooms_create
-  #   broadcast_append_to(@,partial: 'sidebar/single_room',locals: {room: Room.find(room_id)},target: "user_#{user_id}_rooms")
-  # end
 end
